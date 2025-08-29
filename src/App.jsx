5c927e5b33cbb915fe5f67f41b95fac781f66811
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-29 16:58:09 +0530

    Update index.html title and favicon; refactor App component imports for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Navbar from "../components/Navbar.jsx";
-import About from "../components/About.jsx";
-import Work from "../components/Work.jsx";
-import Skills from "../components/Skill.jsx";
-import Contact from "../components/Contact.jsx";
-import Footer from "../components/Footer.jsx";
-import Education from "../components/Education.jsx";
-import Experience from "../components/Experience.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import About from "./components/About/About.jsx";
+import Work from "./components/Work/Work.jsx";
+import Skills from "./components/Skills/Skills.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Education from "./components/Education/Education.jsx";
+import Experience from "./components/Experience/Experience.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen">
+    <div className="bg-[#050414]">
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%, transparent_100%)]"></div>
       <div className="relative pt-20">
         <Navbar />
@@ -24,6 +24,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
