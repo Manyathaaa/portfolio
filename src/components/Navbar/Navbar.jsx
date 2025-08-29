5cc809639c6f88c1b216fac6a7636540d66b6937
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-29 18:36:46 +0530

    Refactor App and Navbar components for improved structure and styling; ensure Navbar has consistent background and menu items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,14 @@ import { FiMenu, FiX } from "react-icons/fi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skill", label: "Skill" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Work" },
+  ];
   return (
-    <nav className="bg-transparent">
+    <nav className="bg-[#050414] w-full">
       <div className="text-white py-5 flex justify-between items-center">
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
@@ -13,6 +19,13 @@ const Navbar = () => {
           <span className="text-white">M</span>
           <span className="text-[#8245ec]">&gt;</span>
         </div>
+        <ul className="md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li key={item.id} className="">
+              <button>{item.label}</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
